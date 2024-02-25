db9ef80b05431f0ac2854335496d46c3574abb23
eventide040 <eventide666@protonmail.com>
2024-02-26 00:01:30 +0100

    feat: implement search

--- Locseng-Web/Program.cs
+++ Locseng-Web/Program.cs
@@ -12,6 +12,8 @@ public class Program
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
 
+        builder.Services.AddSingleton(Indexer.Indexer.Initialize());
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
