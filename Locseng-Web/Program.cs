02f94a01b9c41fc9bafc6a3a3fdc1fe0a8f25cdc
kwilk040 <kamil.wilk@microsoft.wsei.edu.pl>
2024-02-26 21:13:44 +0100

    cleanup: remove unnecessary files

--- Locseng-Web/Program.cs
+++ Locseng-Web/Program.cs
@@ -8,7 +8,6 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
 
@@ -18,11 +17,9 @@ public class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
